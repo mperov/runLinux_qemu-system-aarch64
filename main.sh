3d0a9fcc87160632cb048a69d19758c69953c28d
Maksim Perov <coder@frtk.ru>
2021-03-12 18:23:40 +0300

    all commands are needed to run with root privilege

--- main.sh
+++ main.sh
@@ -8,11 +8,11 @@ wget https://cdimage.debian.org/cdimage/openstack/current/debian-10-openstack-ar
 sudo modprobe nbd
 sudo qemu-nbd -c /dev/nbd0 debian-10-openstack-arm64.qcow2
 sudo mount /dev/nbd0p2 /mnt
-ssh-add -L > /mnt/root/.ssh/authorized_keys
+su -c "ssh-add -L > /mnt/root/.ssh/authorized_keys"
 sudo umount /mnt
 sudo qemu-nbd -d /dev/nbd0
 
-qemu-system-aarch64 -m 2G -M virt -cpu max \
+sudo qemu-system-aarch64 -m 2G -M virt -cpu max \
                     -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd \
                     -drive if=none,file=debian-10-openstack-arm64.qcow2,id=hd0 \
                     -device virtio-blk-device,drive=hd0 \
@@ -20,4 +20,4 @@ qemu-system-aarch64 -m 2G -M virt -cpu max \
                     -netdev user,id=net0,hostfwd=tcp:127.0.0.1:10022-:22 \
                     -nographic
                     
-ssh -p 10022 root@localhost
+sudo ssh -p 10022 root@localhost
